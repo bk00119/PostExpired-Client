d60659994ea31b506798a76f27893c7517b66c33
Kevin Ng <80858693+kn2118@users.noreply.github.com>
2022-06-23 20:31:21 -0400

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Outlet } from "react-router-dom";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav
+        className = "navbar"
+      >
+        <Link to = "/home" className = "nav-items">
+          <button>Home</button>
+        </Link>
+        <Link to = "/browse" className = "nav-items">
+          <button>Browse</button>
+        </Link>
+        <Link to="/stores" className = "nav-items">
+          <button>Stores</button>
+          </Link>
+        <Link to="/saved" className = "nav-items">
+          <button>Saved</button>
+          </Link>
+        <Link to="/more" className = "nav-items">
+          <button>More</button>
+        </Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
-
-export default App;
---
+++ src/routes/home.jsx
@@ -0,0 +1,30 @@
+function Home() {
+    return (
+        <div className = "home">
+            <h1 className = "title">Welcome to Freeload</h1>
+            <h2>Our Mission and Values</h2>
+            <p>Food waste is a huge contributor to climate change accounting for 
+                90% of global CO2 emissions. Our mission here at freeload is to reduce 
+                food waste to nearly 0% by the end of 2030. 
+            </p>
+            <img src = "" alt = "carbon emission"/>
+            <img src = "" alt = "food waste"/>  1
+            <h2>Food Waste</h2>
+            <p> Did you know expired food products are actually not 'expired'. Depending 
+                on the product, food products tend to last way longer their projected expiration 
+                date. Businesses tend to throw away these expired goods despite this resulting
+                in our current global food waste crisis
+            </p>
+            <h2>Our Services</h2>
+            <p>With the help of over 100+ food businesses across the United States, we have established a 
+                database of expired food products in your area that updates in realtime by the second! 
+            </p>
+            <img src = "" alt = "happy businesses"/>
+            <img src = "" alt = "app"/>  
+            <h2>Download our App</h2>
+            <p>Download our app today to form a brighter future, cleaner future for tommorow!</p>
+        </div>
+    )
+}
+
+export default Home; 
