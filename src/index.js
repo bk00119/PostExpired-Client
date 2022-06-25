cecd30c8455f06ff223aad7e1ce9225c6fe72b4b
Kevin Ng <80858693+kn2118@users.noreply.github.com>
2022-06-24 20:07:40 -0400

    Rendering App.js instead of Nav.js (File naming reallocation)  & package.json revisions to fix npm start (website starting issues)

--- src/index.js
+++ src/index.js
@@ -1,42 +1,9 @@
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-
-import Home from "./routes/home"
-import Browse from "./routes/browse"
-import Stores from "./routes/stores";
-import Saved from "./routes/saved";
-import More from "./routes/more";
-import Google from "./components/google"
 import App from "./App"
 
 const root = ReactDOM.createRoot(
   document.getElementById("root")
 );
 root.render(
-  <div>
-    <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<App />} >
-          <Route path = "home" element = {<Home />}/>
-          <Route path = "browse" element = {<Browse />} />
-          <Route path="stores" element={<Stores/>} />
-          <Route path="saved" element={<Saved />} />
-          <Route path = "more" element = {<More/>} />
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>There's nothing here!</p>
-              </main>
-        }
-      />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    <Google />
-  </div>
+    <App />
 );
